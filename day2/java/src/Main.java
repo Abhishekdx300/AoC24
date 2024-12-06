e0b18af1602cfe84dd2c5ef66582bba5ca5d0898
ABHISHEK DAS <abhishekdas300222210@gmail.com>
2024-12-06 23:27:51 +0530

    day 2 part 2 done

--- day2/java/src/Main.java
+++ day2/java/src/Main.java
@@ -1,8 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
@@ -11,38 +9,72 @@ public class Main {
 
         int safeCnt = 0;
 
+        int lineNo = 1;
+
         while(scanner.hasNextLine()){
             List<Integer> nums = Arrays.stream(scanner.nextLine().trim().split("\\s+"))
                     .map(Integer::parseInt)
                     .toList();
 
-            boolean isSafe = true;
-            boolean increasing = nums.get(1) > nums.get(0);
+            // increasing but 1 is lower than prev n next
+            // decreasing but 1 is higher than prev n next
+            // diff between prev n cur <1 or >3
 
-            // pt 2
-            int incWrongCnt = increasing ? 0 : 1;
-            int decWrongCnt = increasing ? 1 : 0;
+            Boolean anySafe = false;
+            
+            if(consider(0,nums)) anySafe = true;
 
-            if(increasing){
-                for(int i=1;i< nums.size();i++){
-                    if(nums.get(i) <= nums.get(i-1) || nums.get(i)- nums.get(i-1)>3){
-                        isSafe=false;
-                        break;
+            for(int i=0;i< nums.size()-1;i++){
+                int diff = nums.get(i+1) - nums.get(i);
+                if(Math.abs(diff) < 1 || Math.abs(diff) > 3){
+                    if(consider(i,nums) || consider(i+1,nums)) anySafe=true;
+                    break;
+                }
+                if(i+2 < nums.size()){
+                    int diff2 = nums.get(i+2) - nums.get(i+1);
+                    // both diff are different in sign ---> valley/pyramid --> any of 3 issue
+                    if( (diff > 0) != (diff2 > 0)){
+                     if(consider(i,nums) || consider(i+1,nums) || consider(i+2,nums) ) anySafe = true;
+                     break;
                     }
                 }
 
-            }else{
-                for(int i=1;i< nums.size();i++){
-                    if(nums.get(i-1) <= nums.get(i) || nums.get(i-1)- nums.get(i)>3){
-                        isSafe=false;
-                        break;
-                    }
-                }
             }
 
-            if(isSafe) safeCnt++;
+            if(anySafe) safeCnt++;
+
+
 
         }
+        System.out.println("**************");
         System.out.println(safeCnt);
+    }
+
+    private static Boolean consider(int idx, List<Integer>nums){
+        List<Integer>newList = new ArrayList<>();
+        for(int i=0;i<nums.size();i++){
+            if(i!=idx) newList.add(nums.get(i));
+        }
+
+        return isSafe(newList);
+    }
+
+    private static boolean isSafe(List<Integer>nums){
+
+        boolean inc = true;
+        boolean dec = true;
+        boolean ok = true;
+
+        for(int i=0;i< nums.size()-1;i++){
+            int diff= nums.get(i+1) - nums.get(i);
+            if(diff>0) dec = false;
+            else if(diff < 0) inc = false;
+            if(Math.abs(diff)< 1 || Math.abs(diff) > 3){
+                ok = false;
+                break;
+            }
+        }
+
+        return ok && (inc || dec);
     }
 }
