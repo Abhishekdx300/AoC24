7737d47123393491fe05e11944458cbf4876e8bb
ABHISHEK DAS <abhishekdas300222210@gmail.com>
2024-12-08 11:47:01 +0530

    day 6 part 2 done

--- day06/java/src/Main.java
+++ day06/java/src/Main.java
@@ -15,6 +15,8 @@ public class Main {
         }
     }
 
+    static Pair[] dirs = {new Pair(-1,0),new Pair(0,1),new Pair(1,0),new Pair(0,-1)};
+
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("input1.txt");
         Scanner scanner = new Scanner(file);
@@ -45,33 +47,16 @@ public class Main {
         }
         int result=0;
 
-        Pair[] dirs = {new Pair(-1,0),new Pair(0,1),new Pair(1,0),new Pair(0,-1)};
-
-        ArrayList<ArrayList<Integer>>vis = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            ArrayList<Integer>temp = new ArrayList<>();
-            for(int j=0;j<m;j++) temp.add(0);
-            vis.add(temp);
-        }
-
-        vis.get(r).set(c,1);
-
-        while(true){
-            int nr = r + dirs[dir].first;
-            int nc = c + dirs[dir].second;
-            if(nr<0 || nr>=n || nc<0 || nc>=m) break;
-            if(matrix.get(nr).get(nc)=='#'){
-                dir = (dir+1)%4;
-            }else{
-                r = nr;
-                c = nc;
-                vis.get(r).set(c,1);
-            }
-        }
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis.get(i).get(j)==1) result++;
+                if(matrix.get(i).get(j)=='.'){
+                    matrix.get(i).set(j,'#');
+                    if(checkCycle(matrix,r,c,dir)){
+                        result++;
+                    }
+                    matrix.get(i).set(j,'.');
+                }
             }
         }
 
@@ -79,4 +64,41 @@ public class Main {
         System.out.println("***********");
         System.out.println(result);
     }
+
+    private static boolean checkCycle(ArrayList<ArrayList<Character>>mat, int r, int c, int dir){
+        int n = mat.size();
+        int m = mat.getFirst().size();
+
+        // make the visited a hashtable????  --- for checking if same r,c,dir is already found
+        // 0 - - - | | | | - - -  -  1   -  -  -  |  |  |  |
+        // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+        // 3*3
+        //***  (row*M+col)*4+dir
+
+
+        ArrayList<Boolean>vis = new ArrayList<>(4*n*m+1);
+        for(int i=0;i<4*n*m+1;i++){
+            vis.add(false);
+        }
+
+        // make 1st pos true
+        vis.set((r*m+c)*4,true);
+
+        while(true){
+            int nr = r + dirs[dir].first;
+            int nc = c + dirs[dir].second;
+            if(nr<0 || nr>=n || nc<0 || nc>=m) break;
+            if(mat.get(nr).get(nc)=='#'){
+                dir = (dir+1)%4;
+            }else{
+                r = nr;
+                c = nc;
+                int hash = (r*m+c)*4+dir;
+                if(vis.get(hash)) return true;
+                vis.set(hash, true);
+            }
+        }
+        return false;
+    }
+
 }
