555da1c41d62edc87edadf1a7104605ee34f882d
ABHISHEK DAS <abhishekdas300222210@gmail.com>
2024-12-08 11:08:23 +0530

    day 6 part 1 better vs.

--- day06/java/src/Main.java
+++ day06/java/src/Main.java
@@ -1,15 +1,23 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Main {
+
+    private static class Pair{
+        public int first;
+        public int second;
+
+        public Pair(int f, int s){
+            this.first = f;
+            this.second = s;
+        }
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("input1.txt");
         Scanner scanner = new Scanner(file);
-
         ArrayList<ArrayList<Character>>matrix = new ArrayList<>(130);
 
         while (scanner.hasNextLine()){
@@ -18,12 +26,11 @@ public class Main {
             for(char ch : line.toCharArray()) temp.add(ch);
             matrix.add(temp);
         }
-        
+
         int n = matrix.size();
         int m = matrix.getFirst().size();
         
         int r=-1,c=-1;
-        
         int dir = 0; // 0- up, 1-right, 2-down, 3-left
         
         for(int i=0;i<n;i++){
@@ -36,48 +43,36 @@ public class Main {
             }
             if(r != -1) break;
         }
+        int result=0;
 
-        matrix.get(r).set(c,'X');
+        Pair[] dirs = {new Pair(-1,0),new Pair(0,1),new Pair(1,0),new Pair(0,-1)};
+
+        ArrayList<ArrayList<Integer>>vis = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            ArrayList<Integer>temp = new ArrayList<>();
+            for(int j=0;j<m;j++) temp.add(0);
+            vis.add(temp);
+        }
+
+        vis.get(r).set(c,1);
+
         while(true){
-            if(dir==0){
-                while(r>0 && matrix.get(r-1).get(c)!='#'){
-                    matrix.get(r-1).set(c,'X');
-                    r--;
-                }
-                if(r==0) break;
-                dir++;
-            }else if(dir==1){
-                while (c<m-1 && matrix.get(r).get(c+1)!='#'){
-                    matrix.get(r).set(c+1,'X');
-                    c++;
-                }
-                if(c==m-1) break;
-                dir++;
-            }else if(dir==2){
-                while (r<n-1 && matrix.get(r+1).get(c)!='#'){
-                    matrix.get(r+1).set(c,'X');
-                    r++;
-                }
-                if(r==n-1) break;
-                dir++;
+            int nr = r + dirs[dir].first;
+            int nc = c + dirs[dir].second;
+            if(nr<0 || nr>=n || nc<0 || nc>=m) break;
+            if(matrix.get(nr).get(nc)=='#'){
+                dir = (dir+1)%4;
             }else{
-                while (c>0 && matrix.get(r).get(c-1)!='#'){
-                    matrix.get(r).set(c-1,'X');
-                    c--;
-                }
-                if(c==0) break;
-                dir=0;
+                r = nr;
+                c = nc;
+                vis.get(r).set(c,1);
             }
         }
 
-        int result=0;
-
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix.get(i).get(j)=='X') result++;
-                System.out.print(matrix.get(i).get(j)+" ");
+                if(vis.get(i).get(j)==1) result++;
             }
-            System.out.println();
         }
 
         System.out.println();
