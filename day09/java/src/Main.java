1b18bf38330a93eef6f639cb7ae50594157ee232
ABHISHEK DAS <abhishekdas300222210@gmail.com>
2024-12-09 12:47:57 +0530

    day 9 part 2 done

--- day09/java/src/Main.java
+++ day09/java/src/Main.java
@@ -4,53 +4,90 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    private static class Filef{
+        public int id;
+        public int index;
+        public int size;
+
+        public Filef(int i, int z, int id){
+            this.index = i;
+            this.size = z;
+            this.id = id;
+        }
+    }
+    private static class Spacef{
+        public int index;
+        public int size;
+
+        public Spacef(int i, int z){
+            this.index = i;
+            this.size = z;
+        }
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("input1.txt");
         Scanner scanner = new Scanner(file);
 
-        ArrayList<Integer>init = new ArrayList<>(200000);
+//        ArrayList<Integer>init = new ArrayList<>(200000);
+
+        // iterate -- store free space starting index and size -- store file id and size and index
+        // iterate from end of file/s and start of space -- if space idx < file idx then store it
+
+        ArrayList<Filef>files = new ArrayList<>();
+        ArrayList<Spacef>spaces = new ArrayList<>();
+
+
 
         int id = 0;
+        int curIdx = 0;
         while (scanner.hasNextLine()){
             String line = scanner.nextLine();
 
             for(int i=0;i<line.length();i++){
-                int k = Integer.parseInt(String.valueOf(line.charAt(i)));
+                int size = Integer.parseInt(String.valueOf(line.charAt(i)));
                 if(i%2==1){
                     // free
-                    for(int j = 0; j< k; j++){
-                        init.add(-1);
-                    }
+                    if(size!=0)
+                        spaces.add(new Spacef(curIdx,size));
                 }else{
-                    for(int j = 0; j< k; j++){
-                        init.add(id);
-                    }
+                    files.add(new Filef(curIdx,size,id));
                     id++;
+                }
+                curIdx+=size;
+            }
+        }
+
+        //compact2
+        int n = files.size();
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<spaces.size();j++){
+                Filef curf = files.get(i);
+                Spacef curs = spaces.get(j);
+                if(curf.index<curs.index){
+                    break;
+                }
+                if(curf.size<=curs.size){
+                    curf.index = curs.index;
+                    // remove curs --- add modified on
+                    int diff = curs.size - curf.size;
+                    if(diff!=0) spaces.set(j,new Spacef(curs.index+curf.size,diff));
+                    else spaces.remove(curs);
+                    break;
                 }
             }
         }
 
-        // compact
-        int n = init.size();
-        int left=0, right=n-1;
-
-        while(left<n && init.get(left)!=-1) left++;
-        while(right>=0 && init.get(right)==-1) right--;
-
-        while(left<n && right>=0 && left<right){
-            init.set(left, init.get(right));
-            init.set(right,-1);
-            while(left<n && init.get(left)!=-1) left++;
-            while(right>=0 && init.get(right)==-1) right--;
-        }
-
         long result = 0L;
 
-        for(int i=0;i<n;i++){
-            int cur = init.get(i);
-            if(cur==-1) break;
-            result+= ((long) i *cur);
+        for(Filef  curf : files){
+            for(int i=0;i<curf.size;i++){
+                result+=((long) curf.id *(curf.index+i));
+            }
         }
+
 
         System.out.println("************");
         System.out.println(result);
